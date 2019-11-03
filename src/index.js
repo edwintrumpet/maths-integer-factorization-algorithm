e829861a6fd0d26daeeeff7db1f0e759f11536e1
Edwin García <spark.com.co@gmail.com>
2019-11-03 12:49:11 -0500

    Agrupación en objetos

--- src/index.js
+++ src/index.js
@@ -4,17 +4,30 @@ const factorization = value => {
     let divisor = 2
     let number = value
     let factors = []
+    let times
     while(divisor<number){
 
         if(number%divisor === 0){
             number = number / divisor
-            factors.push(divisor)
+            if(factors.length !== 0 && (factors[factors.length-1]).number === divisor){
+                times = (factors[factors.length-1]).times + 1;
+                (factors[factors.length-1]).times = times
+            }else{
+                times = 1
+                factors.push({number: divisor, times})
+            }
         }else{
             divisor++
         }
     }
     if(number!==1){
-        factors.push(number)
+        if(factors.length !== 0 && (factors[factors.length-1].number === number)){
+            times = (factors[factors.length-1]).times + 1;
+            (factors[factors.length-1]).times = times
+        }else{
+            times = 1
+            factors.push({number, times})
+        }
     }
     return factors
 }
