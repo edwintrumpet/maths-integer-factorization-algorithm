c2a0987b341efcb1ea5a2fb9885983f75734848c
Edwin García <spark.com.co@gmail.com>
2019-11-03 13:32:55 -0500

    Con 1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-const NUMBER = 360
+const NUMBER = 876543
 
 const factorization = value => {
     let divisor = 2
     let number = value
     let factors = []
-    let times
-    while(divisor<number){
+    let times = 1
+    if(number === 1){
+        factors.push({number, times})
+    }
+    while(divisor < number){
 
-        if(number%divisor === 0){
+        if(number % divisor === 0){
             number = number / divisor
             if(factors.length !== 0 && (factors[factors.length-1]).number === divisor){
                 times = (factors[factors.length-1]).times + 1;
@@ -20,7 +23,7 @@ const factorization = value => {
             divisor++
         }
     }
-    if(number!==1){
+    if(number !== 1){
         if(factors.length !== 0 && (factors[factors.length-1].number === number)){
             times = (factors[factors.length-1]).times + 1;
             (factors[factors.length-1]).times = times
